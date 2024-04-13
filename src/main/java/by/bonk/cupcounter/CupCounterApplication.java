0854a6f62f548e74d6eed915a0d5d9b564074e85
Admin <Ivan.bonk.bio@gmail.con>
2024-04-13 15:51:36 +0300

    Initial commit

---
+++ src/main/java/by/bonk/cupcounter/CupCounterApplication.java
@@ -0,0 +1,13 @@
+package by.bonk.cupcounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CupCounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CupCounterApplication.class, args);
+	}
+
+}
